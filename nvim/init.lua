2aa68923545f065395ac31cb88609c825216b91c
christian <19874293+chpeer@users.noreply.github.com>
2023-01-10 12:01:15 +0000

    updates

---
+++ nvim/init.lua
@@ -0,0 +1,157 @@
+-- source https://bryankegley.me/posts/nvim-getting-started/
+vim.o.termguicolors = true
+vim.o.syntax = 'on'
+vim.o.errorbells = false
+vim.o.smartcase = true
+vim.o.showmode = false
+vim.bo.swapfile = false
+vim.o.backup = false
+vim.o.undodir = vim.fn.stdpath('config') .. '/undodir'
+vim.o.undofile = true
+vim.o.incsearch = true
+vim.o.hidden = true
+vim.o.completeopt='menuone,noinsert,noselect'
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.signcolumn = 'yes'
+vim.wo.wrap = false
+
+-- map leader to space key - that's the key to prefix custom mappings
+vim.g.mapleader = ' '
+
+-- function to set key mappings
+local key_mapper = function(mode, key, result)
+	vim.api.nvim_set_keymap(
+	  mode,
+	  key, 
+	  result,
+	  {noremap = true, silent = true}
+        )
+end
+
+-- custom key mappings
+-- unassign arrow keys
+key_mapper('', '<up>', '<nop>')
+key_mapper('', '<down>', '<nop>')
+key_mapper('', '<left>', '<nop>')
+key_mapper('', '<right>', '<nop>')
+-- map jk as ESC to get out of modes easier
+key_mapper('i', 'jk', '<ESC>')
+key_mapper('i', 'JK', '<ESC>')
+key_mapper('i', 'jK', '<ESC>')
+key_mapper('v', 'jk', '<ESC>')
+key_mapper('v', 'JK', '<ESC>')
+key_mapper('v', 'jK', '<ESC>')
+
+--********
+--plugin management using packer.nvim
+--to install plugins run :PackerCompile and :PackerInstall
+--********
+local vim = vim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute 'packadd packer.nvim'
+end
+vim.cmd('packadd packer.nvim')
+local packer = require'packer'
+local util = require'packer.util'
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+--- startup and add configure plugins
+packer.startup(function()
+  local use = use
+  -- add you plugins here like:
+  -- use 'neovim/nvim-lspconfig'
+  
+  -- treesitter for better syntax highlighting following the AST rather than regex
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'sheerun/vim-polyglot'
+
+  -- these are optional themes but I hear good things about gloombuddy ;)
+  -- colorbuddy allows us to run the gloombuddy theme
+  use 'tjdevries/colorbuddy.nvim'
+  use 'bkegley/gloombuddy'
+  -- sneaking some formatting in here too
+  use {'prettier/vim-prettier', run = 'yarn install' }
+
+  -- plugins to help namage language server protocol client (LSP)
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-lua/completion-nvim'
+  use 'anott03/nvim-lspinstall'
+
+  -- telescope for fuzzy finding
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-lua/telescope.nvim'
+  use 'jremmen/vim-ripgrep'
+  end
+)
+
+-- set default theme
+vim.g.colors_name = 'gloombuddy'
+
+-- configre tree-sitter
+-- install parser for treesitter using :TSInstall{language} - :TSInstallInfo lists all supported languages
+local configs = require'nvim-treesitter.configs'
+configs.setup {
+  ensure_installed = "maintained",
+  highlight = {
+    enable = true,
+  }
+}
+
+-- ------------
+-- configre LSP
+-- ------------
+local lspconfig = require'lspconfig'
+local completion = require'completion'
+local function custom_on_attach(client)
+  print('Attaching to ' .. client.name)
+  completion.on_attach(client)
+end
+local default_config = {
+  on_attach = custom_on_attach,
+}
+-- setup language servers here
+lspconfig.tsserver.setup(default_config)
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+  }
+)
+
+key_mapper('n', 'gd', ':lua vim.lsp.buf.definition()<CR>')
+key_mapper('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>')
+key_mapper('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>')
+key_mapper('n', 'gw', ':lua vim.lsp.buf.document_symbol()<CR>')
+key_mapper('n', 'gW', ':lua vim.lsp.buf.workspace_symbol()<CR>')
+key_mapper('n', 'gr', ':lua vim.lsp.buf.references()<CR>')
+key_mapper('n', 'gt', ':lua vim.lsp.buf.type_definition()<CR>')
+key_mapper('n', 'K', ':lua vim.lsp.buf.hover()<CR>')
+key_mapper('n', '<c-k>', ':lua vim.lsp.buf.signature_help()<CR>')
+key_mapper('n', '<leader>af', ':lua vim.lsp.buf.code_action()<CR>')
+key_mapper('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
+
+-- ------------
+-- configure telescope (fuzzy finding)
+-- ------------
+key_mapper('n', '<C-p>', ':lua require"telescope.builtin".find_files()<CR>')
+key_mapper('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
+key_mapper('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
+key_mapper('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
+
