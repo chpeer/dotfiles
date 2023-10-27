82fb46886393655b54f5425b8f1b68cbbfcfe67d
christian <christian@thoughtmachine.net>
2023-10-27 11:59:45 +0100

    feed back config changes while I was on lazy back into packer

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -1,4 +1,7 @@
-local cmp = require('cmp')
+local ok, cmp = pcall(require,'cmp')
+if not ok then
+  return
+end
 
 cmp.setup({
 	-- the sources to use for autocompletion
@@ -6,7 +9,13 @@ cmp.setup({
 	  { name = 'nvim_lsp' },
 	  { name = 'nvim_lua' },
 	  { name = 'buffer' },
+	  { name = 'luasnip' },
   },
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
   -- update the default sorting order so results don't magically jump around when the autocompletion window is open
   sorting = {
 	  comparators = {
@@ -17,5 +26,48 @@ cmp.setup({
   mapping = {
 	  ['<c-space>'] = cmp.mapping.complete(),
 	  ['<cr>'] = cmp.mapping.confirm({ select = true }),
-  }
+      ['<c-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<c-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<tab>'] = function(fallback)
+        if cmp.visible() then
+          cmp.abort()
+        else
+          fallback()
+        end
+      end,
+      ['<c-n>'] = function()
+        if cmp.visible() then
+          cmp.select_next_item()
+        end
+      end,
+      ['<c-p>'] = function()
+        if cmp.visible() then
+          cmp.select_prev_item()
+        end
+      end,
+  },
+    confirmation = {
+      default_behavior = cmp.ConfirmBehavior.Replace,
+    },
 })
+      
+cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          {
+            name = 'cmdline',
+            option = {
+              ignore_cmds = { 'Man', '!' }
+            }
+          }
+        })
+      })
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -11,6 +11,26 @@ local custom_actions = transform_mod({
   end,
 })
 
+--- Shortens the given path by either:
+--- - making it relative if it's part of the cwd
+--- - replacing the home directory with ~ if not
+---@param path string
+---@return string
+local function shorten_path(path)
+  local cwd = vim.fn.getcwd()
+  if path == cwd then
+    return ''
+  end
+  -- need to escape - since its a special character in lua patterns
+  cwd = cwd:gsub('%-', '%%-')
+  local relative_path, replacements = path:gsub('^' .. cwd .. '/', '')
+  if replacements == 1 then
+    return relative_path
+  end
+  local path_without_home = path:gsub('^' .. os.getenv('HOME'), '~')
+  return path_without_home
+end
+
 telescope.setup({
   defaults = {
     mappings = {
@@ -27,40 +47,222 @@ telescope.setup({
         },
     },
   },
-  pickers = {
-    lsp_document_symbols = {
-            entry_maker = function(entry)
-        local displayer = entry_display.create({
-          separator = ' ',
-          items = {
-            { width = 13 }, -- symbol type
-            { remaining = true }, -- symbol name
+      pickers = {
+        find_files = {
+          layout_config = {
+            width = 0.6,
+            height = 0.9,
           },
-        })
+          previewer = false,
+        },
+        oldfiles = {
+          layout_config = {
+            width = 0.6,
+            height = 0.9,
+          },
+          previewer = false,
+          cwd_only = true,
+          path_display = function(_, path)
+            return shorten_path(path)
+          end,
+        },
+        buffers = {
+          layout_config = {
+            width = 0.6,
+            height = 0.6,
+          },
+          previewer = false,
+          sort_mru = true,
+          ignore_current_buffer = true,
+          mappings = {
+            i = {
+              ['<c-d>'] = 'delete_buffer',
+            },
+            n = {
+              ['<c-d>'] = 'delete_buffer',
+            },
+          },
+        },
+        live_grep = {
+          layout_config = {
+            preview_width = 0.4,
+          },
+        },
+        current_buffer_fuzzy_find = {
+          layout_config = {
+            preview_width = 0.4,
+          },
+        },
+        lsp_document_symbols = {
+          entry_maker = function(entry)
+            local displayer = entry_display.create({
+              separator = ' ',
+              items = {
+                { width = 13 }, -- symbol type
+                { remaining = true }, -- symbol name
+              },
+            })
 
-        local make_display = function(entry)
-          return displayer({
-            { entry.symbol_type, 'CmpItemKind' .. entry.symbol_type },
-            entry.symbol_name,
-          })
-        end
+            local make_display = function(entry)
+              return displayer({
+                { entry.symbol_type, 'CmpItemKind' .. entry.symbol_type },
+                entry.symbol_name,
+              })
+            end
 
-        return {
-          valid = true,
-          value = entry,
-          ordinal = entry.text,
-          display = make_display,
-          filename = entry.filename or vim.api.nvim_buf_get_name(entry.bufnr),
-          lnum = entry.lnum,
-          col = entry.col,
-          symbol_name = entry.text:match('%[.+%]%s+(.*)'),
-          symbol_type = entry.kind,
-          start = entry.start,
-          finish = entry.finish,
-        }
-      end,
-    }
-  }
+            return {
+              valid = true,
+              value = entry,
+              ordinal = entry.text,
+              display = make_display,
+              filename = entry.filename or vim.api.nvim_buf_get_name(entry.bufnr),
+              lnum = entry.lnum,
+              col = entry.col,
+              symbol_name = entry.text:match('%[.+%]%s+(.*)'),
+              symbol_type = entry.kind,
+              start = entry.start,
+              finish = entry.finish,
+            }
+          end,
+        },
+        lsp_dynamic_workspace_symbols = {
+          entry_maker = function(entry)
+            local displayer = entry_display.create({
+              separator = ' ',
+              items = {
+                { width = 13 }, -- symbol type
+                { remaining = true }, -- symbol name
+                { remaining = true }, -- filepath
+              },
+            })
+
+            local make_display = function(entry)
+              return displayer({
+                { entry.symbol_type, 'CmpItemKind' .. entry.symbol_type },
+                entry.symbol_name,
+                { shorten_path(entry.filename), 'TelescopeResultsLineNr' },
+              })
+            end
+
+            return {
+              valid = true,
+              value = entry,
+              ordinal = entry.filename .. entry.text,
+              display = make_display,
+              filename = entry.filename or vim.api.nvim_buf_get_name(entry.bufnr),
+              lnum = entry.lnum,
+              col = entry.col,
+              symbol_name = entry.text:match('%[.+%]%s+(.*)'),
+              symbol_type = entry.kind,
+              start = entry.start,
+              finish = entry.finish,
+            }
+          end,
+        },
+        lsp_references = {
+          entry_maker = function(entry)
+            local displayer = entry_display.create({
+              separator = ' ',
+              items = {
+                { remaining = true }, -- filename
+                { remaining = true }, -- line:col
+                { remaining = true }, -- directory
+              },
+            })
+
+            local make_display = function(entry)
+              return displayer({
+                vim.fs.basename(entry.filename),
+                { entry.lnum .. ':' .. entry.col, 'TelescopeResultsLineNr' },
+                { shorten_path(vim.fs.dirname(entry.filename)), 'TelescopeResultsLineNr' },
+              })
+            end
+
+            return {
+              valid = true,
+              value = entry,
+              ordinal = entry.filename .. entry.text,
+              display = make_display,
+              bufnr = entry.bufnr,
+              filename = entry.filename,
+              lnum = entry.lnum,
+              col = entry.col,
+              text = entry.text,
+              start = entry.start,
+              finish = entry.finish,
+            }
+          end,
+        },
+        lsp_implementations = {
+          entry_maker = function(entry)
+            local displayer = entry_display.create({
+              separator = ' ',
+              items = {
+                { remaining = true }, -- filename
+                { remaining = true }, -- line:col
+                { remaining = true }, -- directory
+              },
+            })
+
+            local make_display = function(entry)
+              return displayer({
+                vim.fs.basename(entry.filename),
+                { entry.lnum .. ':' .. entry.col, 'TelescopeResultsLineNr' },
+                { shorten_path(vim.fs.dirname(entry.filename)), 'TelescopeResultsLineNr' },
+              })
+            end
+
+            return {
+              valid = true,
+              value = entry,
+              ordinal = entry.filename .. entry.text,
+              display = make_display,
+              bufnr = entry.bufnr,
+              filename = entry.filename,
+              lnum = entry.lnum,
+              col = entry.col,
+              text = entry.text,
+              start = entry.start,
+              finish = entry.finish,
+            }
+          end,
+        },
+        lsp_definitions = {
+          entry_maker = function(entry)
+            local displayer = entry_display.create({
+              separator = ' ',
+              items = {
+                { remaining = true }, -- filename
+                { remaining = true }, -- directory
+              },
+            })
+
+            local make_display = function(entry)
+              local head = vim.fs.dirname(entry.filename)
+              local tail = vim.fs.basename(entry.filename)
+              head = shorten_path(head)
+              return displayer({
+                tail,
+                { head, 'TelescopeResultsLineNr' },
+              })
+            end
+
+            return {
+              valid = true,
+              value = entry,
+              ordinal = entry.filename .. entry.text,
+              display = make_display,
+              bufnr = entry.bufnr,
+              filename = entry.filename,
+              lnum = entry.lnum,
+              col = entry.col,
+              text = entry.text,
+              start = entry.start,
+              finish = entry.finish,
+            }
+          end,
+        },
+      }
 })
 
 -- use fuzzy finding
