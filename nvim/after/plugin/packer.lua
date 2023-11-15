79dd6b1230c5b7c5caced5a1bbe3260b0fdd1a8e
christian <christian@thoughtmachine.net>
2023-11-15 00:01:21 +0000

    move packer.lua into plugins folder

---
+++ nvim/after/plugin/packer.lua
@@ -0,0 +1,101 @@
+local ok, packer = pcall(require, 'packer')
+if not ok then
+  print('Packer not installed')
+  return
+end
+
+packer.startup({
+	function(use)
+		use({ 'wbthomason/packer.nvim' })
+
+		use({
+			'pappasam/papercolor-theme-slim',
+			config = function()
+				vim.cmd('colorscheme PaperColorSlim')
+			end
+		})
+
+		use ({
+			'nvim-telescope/telescope.nvim', tag = '0.1.2',
+			-- or                            , branch = '0.1.x',
+			requires = { {'nvim-lua/plenary.nvim'} }
+		})
+		use({
+			'neovim/nvim-lspconfig',
+			requires = {
+				'williamboman/mason.nvim',
+				'williamboman/mason-lspconfig.nvim',
+			},
+		})
+		use({
+			'hrsh7th/nvim-cmp',
+			requires = {
+				"hrsh7th/cmp-nvim-lua",
+				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-buffer",
+				"L3MON4D3/LuaSnip",
+				"saadparwaiz1/cmp_luasnip",
+				"hrsh7th/cmp-cmdline",
+			}
+		})
+		use ({
+			"L3MON4D3/LuaSnip",
+			-- follow latest release.
+			tag = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+			-- install jsregexp (optional!).
+			run = "make install_jsregexp"
+		})
+		use ({'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' })
+		use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
+		use({ 'mbbill/undotree' })
+		use({ 'tpope/vim-fugitive' })
+		use({ 'christoomey/vim-tmux-navigator'})
+		use({ 'sbdchd/neoformat' })
+		use({ 'windwp/nvim-autopairs' })
+		use({ 'ray-x/lsp_signature.nvim' })
+		use({
+			'marcuscaisey/please.nvim',
+			requires = {
+				'nvim-treesitter/nvim-treesitter',
+				'mfussenegger/nvim-dap',
+			},
+		})
+		use({ 'mfussenegger/nvim-dap' })
+		use({ 'theHamsta/nvim-dap-virtual-text' })
+		use({ 'rcarriga/nvim-dap-ui',
+		requires = {
+			"mfussenegger/nvim-dap",
+		},
+	})
+	use({
+		'akinsho/git-conflict.nvim', tag = '*',
+	})
+	use({ 'numToStr/Comment.nvim' })
+	use({
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	})
+	use({'chentoast/marks.nvim'})
+
+	if bootstrap then
+		vim.api.nvim_create_autocmd('User', {
+			group = vim.api.nvim_create_augroup('packer', { clear = true }),
+			pattern = 'PackerComplete',
+			desc = 'Load plugins and configuration after installation',
+			callback = function()
+				vim.ui.select(
+				{ 'Yes', 'No' },
+				{ prompt = "Installed plugins won't be configured until nvim is restarted. Exit?" },
+				function(choice)
+					if choice == 'Yes' then
+						vim.cmd.quitall()
+					end
+				end
+				)
+			end,
+			once = true,
+		})
+		packer.install()
+	end
+end,
+})
