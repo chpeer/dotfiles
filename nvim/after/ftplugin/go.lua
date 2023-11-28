6b18b37bea8e7f4fc675e63abe4b3fdef3d9db74
christian <christian@thoughtmachine.net>
2023-11-28 12:50:16 +0000

    update puku trigger

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -11,14 +11,24 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   pattern = { '*.go' },
   desc = 'Run puku on parent directory',
   callback = function(args)
-    if #vim.fs.find('.plzconfig', { upward = true, path = args.file }) < 1 then
+    if #vim.fs.find('.plzconfig', { upward = true, path = vim.api.nvim_buf_get_name(args.buf) }) < 1 then
       return
     end
-    local r = vim.system({ 'puku', 'fmt', '...' }, { cwd = vim.fs.dirname(args.file) }):wait()
-    if r.code == 0 then
-      print(r.stdout)
-    else
-      print(r.stderr)
+    local function on_event(_, data)
+      local msg = table.concat(data, '\n')
+      msg = vim.trim(msg)
+      msg = msg:gsub('\t', string.rep(' ', 4))
+      if msg ~= '' then
+        vim.notify('puku: ' .. msg, vim.log.levels.INFO)
+      end
     end
+    vim.fn.jobstart({ 'plz', 'puku', 'fmt', '...' }, {
+      cwd = vim.fs.dirname(args.file),
+      on_stdout = on_event,
+      on_stderr = on_event,
+      stdout_buffered = true,
+      stderr_buffered = true,
+    })
   end,
 })
+
