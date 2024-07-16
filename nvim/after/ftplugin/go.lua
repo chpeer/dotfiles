6e703683093f879cee5a2b26bd75a4261d9872bd
christian <19874293+chpeer@users.noreply.github.com>
2024-07-16 13:33:58 +0100

    move puku call into dedicated file

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -1,34 +1,3 @@
 vim.opt_local.textwidth = 120
 vim.opt_local.expandtab = false
 
-if vim.g.loaded_go then
-  return
-end
-vim.g.loaded_go = true
-
-vim.api.nvim_create_autocmd('BufWritePost', {
-  group = vim.api.nvim_create_augroup('go', { clear = true }),
-  pattern = { '*.go' },
-  desc = 'Run puku on parent directory',
-  callback = function(args)
-    if #vim.fs.find('.plzconfig', { upward = true, path = vim.api.nvim_buf_get_name(args.buf) }) < 1 then
-      return
-    end
-    local function on_event(_, data)
-      local msg = table.concat(data, '\n')
-      msg = vim.trim(msg)
-      msg = msg:gsub('\t', string.rep(' ', 4))
-      if msg ~= '' then
-        vim.notify('puku: ' .. msg, vim.log.levels.INFO)
-      end
-    end
-    vim.fn.jobstart({ 'plz', 'puku', 'fmt', '...' }, {
-      cwd = vim.fs.dirname(args.file),
-      on_stdout = on_event,
-      on_stderr = on_event,
-      stdout_buffered = true,
-      stderr_buffered = true,
-    })
-  end,
-})
-
